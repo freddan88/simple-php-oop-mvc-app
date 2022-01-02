e6b5ec21f83ff0e84f070296b2fc1d19b4535f9b
freddan88 <fredrik@leemann.se>
2022-01-02 13:59:35 +0100

    Build router and added routes to index.php

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+require_once(__DIR__ . "/utils/Router.php");
+
+/**
+ * Application routes
+ */
+Router::routeAdd('/', PageController::class, 'index');
+Router::routeAdd('/api', PageController::class, 'api');
+
+// ----------------------
+Router::loadController();
---
+++ app/utils/Router.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+class Router {
+    static $routes = [
+        'default' => [
+            'controllerMethod' => 'default',
+            'controllerName' => PageController::class,
+        ]
+    ];
+
+    private static function getControllerInfo($path)
+    {
+        $controllerInfo = self::$routes[$path];
+
+        if (!$controllerInfo) return self::$routes['default'];
+
+        return $controllerInfo;
+    }
+
+    public static function routeAdd($path, $controller, $method)
+    {
+        self::$routes[$path]['controllerMethod'] = $method;
+        self::$routes[$path]['controllerName'] = $controller;
+    }
+
+    public static function loadController()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $path = parse_url($uri, PHP_URL_PATH);
+
+        $controllerInfo = Self::getControllerInfo($path);
+        $controllerName = $controllerInfo['controllerName'];
+        $controllerMethod = $controllerInfo['controllerMethod'];
+
+        require_once(__DIR__ . "../../controllers/$controllerName.php");
+
+        $controllerName::$controllerMethod();
+    }
+
+}

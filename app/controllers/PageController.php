78964e191c800bd18d01c952bfe7a7b16fe7ca2a
freddan88 <fredrik@leemann.se>
2022-01-02 14:00:12 +0100

    Add controller for the added routes

---
+++ app/controllers/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+require_once(__DIR__ . "../../utils/Response.php");
+
+class PageController {
+
+    public static function index()
+    {
+        $viewData = [
+            'pageTitle' => 'Home',
+            'heading' => 'This is the homepage'
+        ];
+        Response::render('index', $viewData);
+    }
+
+    public static function api()
+    {
+        $jsonData = [
+            'Test' => 'Hello',
+        ];
+        Response::json($jsonData);
+    }
+
+    public static function default()
+    {
+        echo '404 Not Found';
+    }
+}

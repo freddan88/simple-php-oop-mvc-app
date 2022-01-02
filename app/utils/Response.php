d5be5f00ae11299f835fb4a205186d453c3f1424
freddan88 <fredrik@leemann.se>
2022-01-02 14:04:21 +0100

    Add default-value for render-method

--- app/utils/Response.php
+++ app/utils/Response.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 class Response {
 
-    public static function render($viewName, $viewData)
+    public static function render($viewName, $viewData = [])
     {
         $viewData;
         require_once(__DIR__ . "../../views/$viewName.view.php");

fd4b725c7ac328100a332c9c83a26a6d6424825f
freddan88 <fredrik@leemann.se>
2022-01-05 22:33:59 +0100

    Add ability to change where the controller may be loaded from

--- code/app/Router.php
+++ code/app/Router.php
@@ -13,68 +13,71 @@ class Router {
 
     private static $routesArray = [];
 
-    private static function getControllerInfo($path)
+    private static function getControllerInfo($urlPath)
     {
         switch ($_SERVER['REQUEST_METHOD']) {
             case self::GET:
-                $controllerInfo = Self::$routesArray[self::GET][$path];
+                $controllerInfo = Self::$routesArray[self::GET][$urlPath];
                 break;
             case self::PUT:
-                $controllerInfo = Self::$routesArray[self::PUT][$path];
+                $controllerInfo = Self::$routesArray[self::PUT][$urlPath];
                 break;
             case self::POST:
-                $controllerInfo = Self::$routesArray[self::POST][$path];
+                $controllerInfo = Self::$routesArray[self::POST][$urlPath];
                 break;
             case self::DELETE:
-                $controllerInfo = Self::$routesArray[self::DELETE][$path];
+                $controllerInfo = Self::$routesArray[self::DELETE][$urlPath];
                 break;
         }
 
-        if (str_contains($path, 'api') && !$controllerInfo) Response::json(['code' => 404, 'message' => 'Not found']);
+        if (str_contains($urlPath, 'api') && !$controllerInfo) Response::json(['code' => 404, 'message' => 'Not found']);
         if (!$controllerInfo) Response::render('error', ['pageTitle' => '404', 'message' => 'Page not found']);
 
         return $controllerInfo;
     }
 
-    private static function addRoute($requestMethod, $path, $controller, $controllerMethod)
+    private static function addRoute($requestMethod, $urlPath, $controller, $controllerMethod, $controllerPath)
     {
-        Self::$routesArray[$requestMethod][$path]['controllerName'] = $controller;
-        Self::$routesArray[$requestMethod][$path]['controllerMethod'] = $controllerMethod;
+        Self::$routesArray[$requestMethod][$urlPath]['controllerName'] = $controller;
+        Self::$routesArray[$requestMethod][$urlPath]['controllerPath'] = $controllerPath;
+        Self::$routesArray[$requestMethod][$urlPath]['controllerMethod'] = $controllerMethod;
     }
 
-    public static function getRoute($path, $controller, $controllerMethod)
+    public static function getRoute($urlPath, $controller, $controllerMethod, $controllerPath = null)
     {
-        Self::addRoute(self::GET, $path, $controller, $controllerMethod);
+        Self::addRoute(self::GET, $urlPath, $controller, $controllerMethod, $controllerPath);
     }
 
-    public static function putRoute($path, $controller, $controllerMethod)
+    public static function putRoute($urlPath, $controller, $controllerMethod, $controllerPath = null)
     {
-        Self::addRoute(self::PUT, $path, $controller, $controllerMethod);
+        Self::addRoute(self::PUT, $urlPath, $controller, $controllerMethod, $controllerPath);
     }
 
-    public static function postRoute($path, $controller, $controllerMethod)
+    public static function postRoute($urlPath, $controller, $controllerMethod, $controllerPath = null)
     {
-        Self::addRoute(self::POST, $path, $controller, $controllerMethod);
+        Self::addRoute(self::POST, $urlPath, $controller, $controllerMethod, $controllerPath);
     }
 
-    public static function deleteRoute($path, $controller, $controllerMethod)
+    public static function deleteRoute($urlPath, $controller, $controllerMethod, $controllerPath = null)
     {
-        Self::addRoute(self::DELETE, $path, $controller, $controllerMethod);
+        Self::addRoute(self::DELETE, $urlPath, $controller, $controllerMethod, $controllerPath);
     }
 
     public static function loadController()
     {
         $uri = $_SERVER['REQUEST_URI'];
-        $path = parse_url($uri, PHP_URL_PATH);
+        $urlPath = parse_url($uri, PHP_URL_PATH);
 
-        $controllerInfo = Self::getControllerInfo($path);
+        $controllerInfo = Self::getControllerInfo($urlPath);
         $controllerName = $controllerInfo['controllerName'];
+        $controllerPath = $controllerInfo['controllerPath'];
         $controllerMethod = $controllerInfo['controllerMethod'];
-        
-        require_once(__DIR__ . "../../controllers/$controllerName.php");
+
+        $controllerPath = $controllerPath ? 'controllers/' . trim($controllerPath,'/') : 'controllers';
+        require_once(__DIR__ . "../../$controllerPath/$controllerName.php");
         
         if (!method_exists($controllerName, $controllerMethod)) {
-            if (str_contains($path, 'api')) Response::json(['code' => 501, 'message' => 'Not Implemented']);
+            if (str_contains($urlPath, 'api')) Response::json(['code' => 501, 'message' => 'Not Implemented']);
             Response::render('error', ['pageTitle' => '501', 'message' => 'Not Implemented']);
         }
 

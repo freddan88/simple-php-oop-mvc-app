74c6060585008df643933c199ecfd2c4e31ebf42
freddan88 <fredrik@leemann.se>
2022-01-06 18:39:41 +0100

    Experimenting with authentification

--- code/index.php
+++ code/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 require_once(__DIR__ . "/app/Router.php");
+require_once(__DIR__ . "/app/utils/ProtectRoute.php");
 
 // (www) get-routes
 Router::getRoute('/', PageController::class, 'index');
@@ -10,12 +11,12 @@ Router::getRoute('/login', PageController::class, 'login');
 Router::getRoute('/signup', PageController::class, 'signup');
 
 // (www) post-routes
-Router::postRoute('/login', UserController::class, 'login');
+ProtectRoute::isLogedin('/login') && Router::postRoute('/login', UserController::class, 'login');
 Router::postRoute('/signup', UserController::class, 'signup');
 Router::postRoute('/logout', UserController::class, 'logout');
 
 // (api) get-routes
-Router::getRoute('/api', ApiController::class, 'index');
+ProtectRoute::apiKey() && Router::getRoute('/api', ApiController::class, 'index');
 
 /**
  * Supported http-methods and examples:

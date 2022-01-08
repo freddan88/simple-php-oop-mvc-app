c82e3d1ae08725988b4b9cd2ee7c0ffe2251dafd
freddan88 <fredrik@leemann.se>
2022-01-08 21:28:40 +0100

    Delleted unused code in view

--- code/views/error.view.php
+++ code/views/error.view.php
@@ -1,7 +1,6 @@
 <?php require_once(__DIR__ . "/_partials/head.part.php") ?>
 <body>
-  <p><?= $viewData->code ?></p>
-  <h1><?= $viewData->pageTitle ?></h1>
+  <h1><?= $viewData->code ?></h1>
   <h2><?= $viewData->message ?></h2>
 </body>
 </html>

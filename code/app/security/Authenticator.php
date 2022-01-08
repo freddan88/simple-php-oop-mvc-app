8a0cb631dbaa777ffb92419598ca4a2868cfb91e
freddan88 <fredrik@leemann.se>
2022-01-08 12:17:41 +0100

    Delete unused classes

--- code/app/security/Authenticator.php
+++ code/app/security/Authenticator.php
@@ -46,3 +46,54 @@ class Authenticator {
         }
     }
 }
+
+// class ProtectRoute {
+
+//     public static function isLogedin($path)
+//     {
+//         $uri = $_SERVER['REQUEST_URI'];
+//         $urlPath = parse_url($uri, PHP_URL_PATH);
+//         $urlPath = '/' . trim($urlPath,'/');
+//         if ($urlPath !== $path) return true;
+//         if (isset($_SESSION['isLogedin']) && $_SESSION['isLogedin']) return true;
+//         return false;
+//     }
+
+//     public static function bearer()
+//     {
+//         if (empty($_SERVER['HTTP_AUTHORIZATION'])) return false;
+//         $bearerAuthString = $_SERVER['HTTP_AUTHORIZATION'];
+//         $bearerAuth = explode(' ', $bearerAuthString);
+//         if ($bearerAuth[0] !== 'Bearer') return false;
+//         if ($bearerAuth[1] === '123') return true;
+//         return false;
+//     }
+
+//     public static function apiKey()
+//     {
+//         if (empty($_GET['key'])) return false;
+//         if ($_GET['key'] === '123') return true;
+//         return false;
+//     }
+// }
+
+// Authentication is the process of verifying who someone is
+// Authorization is the process of verifying what specific applications, files, and data a user has access to.
+
+// class Security {
+
+//     public function authenticated($path)
+//     {
+//         $uri = $_SERVER['REQUEST_URI'];
+//         $urlPath = parse_url($uri, PHP_URL_PATH);
+//         $urlPath = '/' . trim($urlPath,'/');
+//         if ($urlPath !== $path) return true;
+//         if (isset($_SESSION['isLogedin']) && $_SESSION['isLogedin']) return true;
+//         return false;
+//     }
+
+//     public function authorized()
+//     {
+//         return false;
+//     }
+// }

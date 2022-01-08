25d7d2bc7f3f8b9e20857892927f6d4cb8e2833b
freddan88 <fredrik@leemann.se>
2022-01-08 14:59:24 +0100

    Experimenting with database-querybuilder

--- code/app/database/Database.php
+++ code/app/database/Database.php
@@ -4,12 +4,114 @@ declare(strict_types=1);
 
 Trait Database {
     
-    private $databaseConnection;
+    private $pdo;
+    private $sql;
+    private $statement;
+
+    private function getFieldList($fieldArray, $separator = ', ')
+    {
+        $fields = [];
+        foreach ($fieldArray as $field) {
+            array_push($fields, "$field = :$field");
+        }
+        return implode($separator, $fields);
+    }
+
+    private function getWhereList($fieldArray, $operator, $separator)
+    {
+        $fields = [];
+        foreach ($fieldArray as $field => $value) {
+            array_push($fields, "$field $operator $value");
+        }
+        return implode($separator, $fields);
+    }
 
     public function sqlite($databaseName = 'sqlite')
     {
         $databasePath = sprintf('sqlite:%s/database_%s.db', __DIR__, $databaseName);
-        $this->databaseConnection = new PDO($databasePath);
+        $this->pdo = new PDO($databasePath);
         return $this;
+    }
+
+    public function get($fields)
+    {
+        $fieldList = $this->getFieldList($fields);
+        $this->sql = "SELECT $fieldList FROM ";
+        return $this;
+    }
+
+    public function getAll()
+    {
+        $this->sql = 'SELECT * FROM ';
+        return $this;
+    }
+
+    public function update($tableName)
+    {
+        $this->sql = "UPDATE $tableName SET ";
+        return $this;
+    }
+
+    public function table($table)
+    {
+        $this->sql .= "$table ";
+        return $this;
+    }
+
+    public function fields($fields)
+    {
+        $fieldList = $this->getFieldList($fields);
+        $this->sql .= "$fieldList ";
+        return $this;
+    }
+
+    public function where($fields, $operator = '=', $separator = ' AND ')
+    {
+        $fieldList = $this->getWhereList($fields, $operator, $separator);
+        $this->sql .= "WHERE $fieldList";
+        return $this;
+    }
+
+    public function prepare()
+    {
+        $this->sql = trim($this->sql) . ';';
+        $this->statement = $this->pdo->prepare($this->sql);
+        if (!$this->statement) die(var_dump($this->pdo->errorInfo()));
+        return $this;
+    }
+
+    public function bind($fieldName, $fieldValue, $pdoBindOption)
+    {
+        $this->statement->bindParam(":$fieldName", $fieldValue, $pdoBindOption);
+        return $this;
+    }
+
+    public function execute()
+    {
+        $this->statement->execute();
+        return $this;
+    }
+
+    public function fetch($pdoFetchOption = PDO::FETCH_OBJ)
+    {
+        return $this->statement->fetch($pdoFetchOption);
+    }
+
+    public function fetchAll($pdoFetchOption = PDO::FETCH_OBJ)
+    {
+        return $this->statement->fetchAll($pdoFetchOption);
+    }
+
+    public function debug()
+    {
+        echo '<pre>', var_dump($this->statement), '</pre>';
+        exit;
+    }
+
+    public function debugSql()
+    {
+        $this->sql = trim($this->sql) . ';';
+        echo '<pre>', var_dump($this->sql), '</pre>';
+        exit;
     }
 }
--- code/models/User.php
+++ code/models/User.php
@@ -9,6 +9,14 @@ class User {
 
     public function __construct()
     {
-        $this->sqlite();
+        $fields = ['name', 'email', 'password', 'role'];
+        $value = $this->sqlite()->getAll()->table('tbl_users')->prepare()->execute()->fetch();
+        // $this->sqlite()->prepare()
+        // ->bind($fields[0], 'Fredrik Leemann', PDO::PARAM_STR)
+        // ->bind($fields[1], 'fredrik@leemann.se', PDO::PARAM_STR)
+        // ->bind($fields[2], '123', PDO::PARAM_STR)
+        // ->bind($fields[3], '123', PDO::PARAM_INT)
+        // ->execute();
+        die(var_dump($value->name));
     }
 }
